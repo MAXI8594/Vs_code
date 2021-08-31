a2abbfc2464ee4145f1770b6b2d69ecd6f559557
Maxy <maxioliverauni@gmail.com>
2021-08-31 12:05:09 -0300

    commit iniciar

---
+++ Master en penetration testing y ethical hacking con python 3/1-String.py
@@ -0,0 +1,8 @@
+nombre = "Pedro"
+
+print(nombre.upper())
+
+nombre2 = "pedro"
+print (nombre2[1]) 
+print(len(nombre)-1)
+print(nombre[0:3])
---
+++ Master en penetration testing y ethical hacking con python 3/2-In-out.py
@@ -0,0 +1,7 @@
+num = int(input("ingrese el primer num:\n>>"))
+num2 = int(input("ingrese el segundo num:\n>>"))
+
+suma = (num+num2)
+print("la suma "+str(num )+"  mas  "+str(num2)+" s igual a  "+ str(num+num2))
+print("la suma de {} con {} es igual a {}".format(num,num2,suma))
+print(f"la suma de {num} con {num2} es igual a {suma}")
